d9088aafad4aa626b74ab9fc84baf460b7a7a0f5
Pawel Nackowski <pawelnackowski@gmail.com>
2022-11-10 11:42:31 +0100

    add function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-console.log("Witam wszystkich co tutaj zaglądają 😀");
+{
+    const welcome = () => {
+        console.log("Witam wszystkich co tutaj zaglądają 😀");
+    }
 
-let buttonTable = document.querySelector(".js-buttonTable");
-let buttonText = document.querySelector(".js-buttonText");
-let tableHiden = document.querySelector(".js-hiden");
+    const onShowTableOnClick = () => {
+        const buttonText = document.querySelector(".js-buttonText");
+        const tableHiden = document.querySelector(".js-hiden");
 
-buttonTable.addEventListener("click", () => {
-    tableHiden.classList.toggle("hiden");
+        tableHiden.classList.toggle("hiden");
+        buttonText.innerText = tableHiden.classList.contains("hiden") ? "Pokaż tabelę" : "Ukryj Tabelę";
+    };
 
-    buttonText.innerText = tableHiden.classList.contains("hiden") ? "Pokaż tabelę" : "Ukryj Tabelę";
-});
+    const init = () => {
+        const buttonTable = document.querySelector(".js-buttonTable");
+
+        buttonTable.addEventListener("click", onShowTableOnClick);
+    }
+
+    welcome();
+    init();
+}
